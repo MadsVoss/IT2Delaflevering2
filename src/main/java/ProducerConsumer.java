b981d45ea242506cd0e3446b09b4d23119fd5b19
mikkelbech <43571494+mikkelbech@users.noreply.github.com>
2020-06-11 12:20:39 +0200

    lavet om i sensor og PC

--- src/main/java/ProducerConsumer.java
+++ src/main/java/ProducerConsumer.java
@@ -1,12 +1,13 @@
 import java.util.LinkedList;
 
-// This class has a list, producer (adds items to list
+// This class has a listgui, producer (adds items to listgui
 // and consumber (removes items).
 public class ProducerConsumer {
     Sensor sensor;
-    // Create a list shared by producer and consumer
-    // Size of list is 2.
-    LinkedList<Integer> list = new LinkedList<>();
+    // Create a listgui shared by producer and consumer
+    // Size of listgui is 2.
+    LinkedList<Integer> listgui = new LinkedList<>();
+    LinkedList<Integer> listdb = new LinkedList<>();
     int capacity = 1000;
 
     // Function called by producer thread
@@ -14,16 +15,16 @@ public class ProducerConsumer {
 
         while (true) {
             synchronized (this) {
-                // producer thread waits while list
+                // producer thread waits while listgui
                 // is full
-                while (list.size() == capacity)
+                while (listgui.size() == capacity)
                     wait();
-                int value = Integer.parseInt(sensor.getData());
+                int value = sensor.getData();
                 //System.out.println("Producer produced-"
                 //        + value);
 
-                // to insert the jobs in the list
-                list.add(value);
+                // to insert the jobs in the listgui
+                listgui.add(value);
 
                 // notifies the consumer thread that
                 // now it can start consuming
@@ -40,16 +41,15 @@ public class ProducerConsumer {
     public void consume() throws InterruptedException {
         while (true) {
             synchronized (this) {
-                // consumer thread waits while list
+                // consumer thread waits while listgui
                 // is empty
-                while (list.size() < 50)
+                while (listgui.size() < 50)
                     wait();
 
-                // to retrive the ifrst job in the list
-                LinkedList<Integer> removedObject = new LinkedList<>();
-                list.removeAll(removedObject);
-
-                // System.out.println("Consumer consumed-");
+                // to retrive the ifrst job in the listgui
+                LinkedList<Integer> removedObject = listgui;
+                listgui = new LinkedList<>();
+                listgui.removeAll(removedObject);
 
                 // Wake up producer thread
                 notify();
@@ -64,16 +64,15 @@ public class ProducerConsumer {
     public void consume2() throws InterruptedException {
         while (true) {
             synchronized (this) {
-                // consumer thread waits while list
+                // consumer thread waits while listgui
                 // is empty
-                while (list.size() < 50)
+                while (listgui.size() < 100)
                     wait();
 
-                // to retrive the ifrst job in the list
-                LinkedList<Integer> removedObject = new LinkedList<>();
-                list.removeAll(removedObject);
-
-                System.out.println("Consumer consumed-");
+                // to retrive the ifrst job in the listgui
+                LinkedList<Integer> removedObject = listdb;
+                listdb = new LinkedList<>();
+                listdb.removeAll(removedObject);
 
                 // Wake up producer thread
                 notify();
@@ -81,6 +80,15 @@ public class ProducerConsumer {
                 // and sleep
                 Thread.sleep(1000);
             }
+        }
+    }
+
+    public void addToQueue(int data) {
+        synchronized (this){
+            this.listgui.add(data);
+            this.listdb.add(data);
+            notify();
+
         }
     }
 }
--- src/main/java/Sensor.java
+++ src/main/java/Sensor.java
@@ -4,6 +4,7 @@ public class Sensor {
     //Opretter SerialPort så vi senere kan gå ind og få fat i det hvor pulsmåleren sidder. Derefter opretter vi en string som vi kan sætte et resultat ind i.
     private SerialPort serialPort = null;
     private String result = null;
+    private int dataSerialport = 0;
 
 
     //Denne finder ud af hvor sensoren er plugget til og "snakker" med den
@@ -23,17 +24,27 @@ public class Sensor {
     }
 
     //Her går vi ind og finder selve dataen fra pulsmåleren så vi kan bruge det inde i Monitor klassen.
-    public String getData() {
+    public int getData() {
         try {
             //For den her til at "sove" et stykke tid så vi sørger for den snakker ordentligt sammen med vores Arduino kode
             Thread.sleep(20);
             //Her siger vi at vi kun vil have resultatet hvis den string fra Arduino er 14 tegn eller længere. Så vi sikrer at vi ikke får en masse fejlmålinger som 0 taller osv med ind.
-            if (serialPort.getInputBufferBytesCount() >= 14) {
+            if (serialPort.getInputBufferBytesCount() > 0) {
                 result = serialPort.readString();
+                dataSerialport = Integer.parseInt(result.substring(result.indexOf(""), result.indexOf("#")));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
+        return dataSerialport;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Sensor sensor = new Sensor(0);
+        while(true){
+            Thread.sleep(10);
+            int data = sensor.getData();
+            System.out.println(data);
+        }
     }
 }
